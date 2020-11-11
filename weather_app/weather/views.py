fc1e80edc29fa0107dcf4b639fd17e125a0017a1
Sergey K <serjkasmail@gmail.com>
2020-11-11 21:28:15 +0300

    add cities to db

--- weather_app/weather/views.py
+++ weather_app/weather/views.py
@@ -1,19 +1,26 @@
 from django.shortcuts import render
 import requests
-
+from .models import City
 
 def index(request):
     appid = '33babf34df7f7a714bc1812bb8445f04'
-    city = 'Vladimir'
-    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={appid}'
-    res = requests.get(url).json()
-    city_info = {
-        'city': city,
-        'temp': res["main"]["temp"],
-        'icon': res["weather"][0]["icon"]
-    }
-    print(city_info)
+
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid='+appid
+
+    cities = City.objects.all()
+
+    all_cities = []
+
+    for city in cities:
+        res = requests.get(url.format(city.name)).json()
+        city_info = {
+            'city': city.name,
+            'temp': res["main"]["temp"],
+            'icon': res["weather"][0]["icon"]
+        }
+        all_cities.append(city_info)
+
     context = {
-        'info': city_info
+        'all_info': all_cities
     }
     return render(request, 'weather/index.html', context)
