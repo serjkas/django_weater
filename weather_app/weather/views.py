89807b3ca24a4e851c07bb4a8fe10604c97a3bc8
Sergey K <serjkasmail@gmail.com>
2020-11-11 20:59:07 +0300

    take json and print values

--- weather_app/weather/views.py
+++ weather_app/weather/views.py
@@ -4,8 +4,16 @@ import requests
 
 def index(request):
     appid = '33babf34df7f7a714bc1812bb8445f04'
-    city = 'London'
-    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={appid}'
-    res = requests.get(url)
-    print(res.text)
-    return render(request, 'weather/index.html')
+    city = 'Vladimir'
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={appid}'
+    res = requests.get(url).json()
+    city_info = {
+        'city': city,
+        'temp': res["main"]["temp"],
+        'icon': res["weather"][0]["icon"]
+    }
+    print(city_info)
+    context = {
+        'info': city_info
+    }
+    return render(request, 'weather/index.html', context)
