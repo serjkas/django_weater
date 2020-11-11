9d68ab5b4ec811ebb4d67ee9f684a21969d01a53
Sergey K <serjkasmail@gmail.com>
2020-11-11 20:45:09 +0300

    get api from weathersite and test get weather

--- weather_app/weather/views.py
+++ weather_app/weather/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+import requests
 
 
 def index(request):
+    appid = '33babf34df7f7a714bc1812bb8445f04'
+    city = 'London'
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={appid}'
+    res = requests.get(url)
+    print(res.text)
     return render(request, 'weather/index.html')
